f865cbbfebf978e943ea4e5aa51a8fe772a98e2e
Lemon <101458752+LemonLabours@users.noreply.github.com>
2023-08-24 21:47:36 +0300

    Added IntroView

--- lib/app/Core/bloc/bloc/booking_bloc.dart
+++ lib/app/Core/bloc/bloc/booking_bloc.dart
@@ -1,4 +1,5 @@
 import 'package:bloc/bloc.dart';
+// ignore: depend_on_referenced_packages
 import 'package:meta/meta.dart';
 
 part 'booking_event.dart';
@@ -7,6 +8,7 @@ part 'booking_state.dart';
 class BookingBloc extends Bloc<BookingEvent, BookingState> {
   BookingBloc() : super(BookingInitial()) {
     on<BookingEvent>((event, emit) {
+      // ignore: todo
       // TODO: implement event handler
     });
   }
--- lib/app/Views/Intro/intro_view.dart
+++ lib/app/Views/Intro/intro_view.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-
 import '../../Core/utilities/constants/colors.dart';
 
 class IntroView extends StatelessWidget {
@@ -12,7 +11,35 @@ class IntroView extends StatelessWidget {
         decoration: const BoxDecoration(
           gradient: babyAndDarkBlueGradient,
         ),
-        // Rest of your scaffold's content here...
+        child: Center(
+          child: ElevatedButton.icon(
+            onPressed: () {},
+            icon: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Image.asset('assets/imgs/google-logo.png',
+                  height: 40, width: 40),
+            ),
+            label: const Text(
+              'Sign in with Google',
+              style: TextStyle(
+                fontSize: 20,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            style: ElevatedButton.styleFrom(
+              foregroundColor: Colors.black,
+              backgroundColor: Colors.white,
+              padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 12),
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(30),
+              ),
+              side: const BorderSide(
+                color: Colors.black,
+                width: 2,
+              ),
+            ),
+          ),
+        ),
       ),
     );
   }
