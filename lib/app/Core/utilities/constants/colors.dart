dbc8465fa69daf79ed9a124ac01fac985d32a155
asma <asma.alkathiiri@gmail.com>
2023-08-24 11:45:06 +0300

    added colors

---
+++ lib/app/Core/utilities/constants/colors.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+const primary = Color(0xFF5d78ff);
+const secondary = Color(0xFF23bffe);
+
+const hMnueColor = Color(0xFFe1edff);
+const textColor = Color(0xFF4b4e58);
+const hLightGray = Color(0xFFb1b5c1);
+const hDarkGray = Color(0xFF5e6069);
+
+
