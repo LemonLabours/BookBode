a7ae8966455069d79c43d2bd7c152a3a0c8ffb27
asma <asma.alkathiiri@gmail.com>
2023-08-25 15:07:05 +0300

    Added shared files

---
+++ lib/app/Core/utilities/shared/buttons_widgets.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+import 'package:project_3/app/Core/utilities/constants/colors.dart';
+import 'package:project_3/app/Core/utilities/constants/spacing.dart';
+
+class ButtonsWidgets extends StatelessWidget {
+  const ButtonsWidgets(
+      {super.key, this.onPressed, required this.text, this.icons});
+  final Function()? onPressed;
+  final String text;
+  final IconData? icons;
+
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton(
+      style: ElevatedButton.styleFrom(
+        elevation: 0,
+        backgroundColor: hWhiteColor,
+        side: const BorderSide(color: hLightGray, style: BorderStyle.solid),
+        fixedSize: const Size(380, 50),
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(50),
+        ),
+      ),
+      onPressed: onPressed,
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Icon(
+            icons,
+            color: hBlachColor,
+          ),
+          kHSpace16,
+          Text(
+            text,
+            style: const TextStyle(
+              color: Colors.black,
+              fontSize: 18,
+              fontWeight: FontWeight.w400,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
